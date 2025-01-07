e6f84334dab6245373ceb645377cbd0e177170cf
Loris Leiva <loris.leiva@gmail.com>
2025-01-07 12:06:01 +0100

    Add processor folder

---
+++ program/src/processor/create.rs
@@ -0,0 +1,53 @@
+use crate::{
+    assertions::{assert_pda, assert_same_pubkeys, assert_signer, assert_writable},
+    instruction::accounts::CreateAccounts,
+    state::{Counter, Key},
+    utils::create_account,
+};
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, system_program};
+
+pub fn create<'a>(accounts: &'a [AccountInfo<'a>]) -> ProgramResult {
+    // Accounts.
+    let ctx = CreateAccounts::context(accounts)?;
+
+    // Guards.
+    let counter_bump = assert_pda(
+        "counter",
+        ctx.accounts.counter,
+        &crate::ID,
+        &Counter::seeds(ctx.accounts.authority.key),
+    )?;
+    assert_signer("authority", ctx.accounts.authority)?;
+    assert_signer("payer", ctx.accounts.payer)?;
+    assert_writable("payer", ctx.accounts.payer)?;
+    assert_same_pubkeys(
+        "system_program",
+        ctx.accounts.system_program,
+        &system_program::id(),
+    )?;
+
+    // Do nothing if the domain already exists.
+    if !ctx.accounts.counter.data_is_empty() {
+        return Ok(());
+    }
+
+    // Create Counter PDA.
+    let counter = Counter {
+        key: Key::Counter,
+        authority: *ctx.accounts.authority.key,
+        value: 0,
+    };
+    let mut seeds = Counter::seeds(ctx.accounts.authority.key);
+    let bump = [counter_bump];
+    seeds.push(&bump);
+    create_account(
+        ctx.accounts.counter,
+        ctx.accounts.payer,
+        ctx.accounts.system_program,
+        Counter::LEN,
+        &crate::ID,
+        Some(&[&seeds]),
+    )?;
+
+    counter.save(ctx.accounts.counter)
+}
