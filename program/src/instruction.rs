72e286111018a686d20679128e2a876812f04332
febo <febo@anza.xyz>
2025-01-09 20:11:35 +0000

    [wip]: Update comments

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,43 +1,112 @@
 use pinocchio::program_error::ProgramError;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 pub enum ProgramMetadataInstruction {
     /// Writes data to a pre-funded (buffer) account.
     ///
-    /// This instruction allocated and assign the account to the
-    /// program if needed.
+    /// The buffer account must be pre-funded with enough lamports to cover
+    /// the storage cost of the data being written. It also needs to be
+    /// assigned to the program.
     ///
-    /// 0. `[s,w]` The pre-funded account to write.
+    /// ### Accounts
+    ///  0. `[ w,s ]` The buffer account to write.
     ///
-    /// Instruction data: bytes to write.
+    /// ### Instruction data
+    ///  - `[u8]`:  bytes
     Write,
 
-    /// Initializes a canonical metadata account.
+    /// Initializes a metadata account.
     ///
     /// A canonical metadata account is an account initialized
     /// by the program upgrade authority.
     ///
-    /// 0. `[w]` The metadata account to initialize.
-    /// 1. `[w,o]` (Optional) Buffer account to copy data from.
-    /// 2. `[s]` The program upgrade authority.
-    /// 3. `[ ]` Program account.
-    /// 4. `[ ]` Program data account.
-    /// 5. `[ ]` System program.
+    /// ### Accounts
+    ///  0. `[  w  ]` The metadata account to initialize.
+    ///  1. `[ w,o ]` (optional) Buffer account to copy data from.
+    ///  2. `[  s  ]` The program upgrade authority.
+    ///  3. `[     ]` Program account.
+    ///  4. `[     ]` Program data account.
+    ///  5. `[     ]` System program.
+    ///
+    /// ### Instruction data
+    ///  - `[u8; 17]`: seed
+    ///  - `u8`: encoding
+    ///  - `u8`: compression
+    ///  - `u8`: format
+    ///  - `u8`: data source
+    ///  - `[u8]`: (optional) bytes
     Initialize,
 
     /// Sets the authority of a metadata account.
+    ///
+    /// When setting the authority to a `canonical` metadata account with the
+    /// program upgrade authority, both program and program data accounts are
+    /// required.
+    ///
+    /// Note: It is not possible to set an authority if the account is immutable.
+    ///
+    /// ### Accounts
+    ///  0. `[  w  ]` Metadata account.
+    ///  1. `[  s  ]` Current authority account.
+    ///  2. `[  o  ]` (optional) Program account.
+    ///  3. `[  o  ]` (optional) Program data account.
+    ///
+    /// ### Instruction data
+    ///  - `u8`: option (0 = remove authority, 1 = set authority)
+    /// - `[u8; 32]`: (optional) new authority
     SetAuthority,
 
-    /// Updates the data of a metadata account from an buffer account.
+    /// Sets the data and its metadata.
+    ///
+    /// The data can be provided as instruction data or copied from a buffer
+    /// account. When setting the authority to a `canonical` metadata account
+    /// with the program upgrade authority, both program and program data
+    /// accounts are required.
+    ///
+    /// Note: It is not possible to set data if the account is immutable.
+    ///
+    /// ### Accounts
+    ///  0. `[  w  ]` Metadata account.
+    ///  1. `[  s  ]` Authority account.
+    ///  2. `[ w,o ]` (optional) Buffer account to copy data from.
+    ///  3. `[  o  ]` (optional) Program account.
+    ///  4. `[  o  ]` (optional) Program data account.
+    ///
+    /// ### Instruction data
+    ///  - `u8`: encoding
+    ///  - `u8`: compression
+    ///  - `u8`: format
+    ///  - `u8`: data source
+    ///  - `[u8]`: (optional) bytes
     SetData,
 
     /// Sets the metadata account as immutable.
+    ///
+    /// ### Accounts
+    ///  0. `[  w  ]` Metadata account.
+    ///  1. `[  s  ]` Authority account.
     SetImmutable,
 
     /// Withdraws excess lamports from a metadata account.
+    ///
+    /// ### Accounts
+    ///  0. `[  w  ]` Metadata account.
+    ///  1. `[  s  ]` Authority account.
+    ///  2. `[  w  ]` Destination account.
     WithdrawExcessLamports,
 
     /// Closes a metadata account.
+    ///
+    /// The lamports in the metadata account are transferred to the destination
+    /// account.
+    ///
+    /// Note: It is not possible to close a metadata account if the account
+    /// is immutable.
+    ///
+    /// ### Accounts
+    ///  0. `[  w  ]` Metadata account.
+    ///  1. `[  s  ]` Authority account.
+    ///  2. `[  w  ]` Destination account.
     Close,
 }
 
