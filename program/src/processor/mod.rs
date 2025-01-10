360dd9371cc4ce5b4dc4cc74a99f0ff1e6bd32f8
Loris Leiva <loris.leiva@gmail.com>
2025-01-10 10:54:40 +0100

    [wip]: Refactor `is_program_authority` helper

--- program/src/processor/mod.rs
+++ program/src/processor/mod.rs
@@ -9,56 +9,64 @@ pub mod set_immutable;
 pub mod withdraw_excess_lamports;
 pub mod write;
 
+/// Checks if the provided `authority` is the authority allowed to update the `program`.
+/// Fails when providing unexpected input.
+///
+/// ## Validation
+/// The following validation checks are performed:
+///
+/// - [explicit] The `program` account must be executable.
+/// - [explicit] The `program` account discriminator (first byte) must be `2` — i.e. defining a `Program` account.
+/// - [explicit] The `program_data` account must be the one set on the `program` account data.
+/// - [explicit] The `program_data` account must not be executable.
+/// - [explicit] The `program_data` account discriminator (first byte) must be `3` — i.e. defining a `ProgramData` account.
+/// - [explicit] The `program_data` account must have 32 bytes of data in the range [13..45], representing the authority.
 #[inline(always)]
 fn is_program_authority(
     program: &AccountInfo,
     program_data: &AccountInfo,
     authority: &Pubkey,
 ) -> Result<bool, ProgramError> {
-    // program
-    // - must be executable
-
-    let program_account_data = unsafe { program.borrow_data_unchecked() };
-
-    let program_data_key = match (program_account_data.first(), program.executable()) {
-        // 2 - program
-        (Some(2), true) => {
-            // offset = 4 (discriminator)
-            let program_data_key: Pubkey = program_account_data[4..]
-                .try_into()
-                .map_err(|_| ProgramError::InvalidAccountData)?;
-            program_data_key
-        }
-        _ => {
-            // TODO: use custom error (invalid program state)
-            return Err(ProgramError::InvalidAccountData);
+    // Program checks.
+    let expected_program_data = {
+        let data = unsafe { program.borrow_data_unchecked() };
+        match (data.first(), program.executable()) {
+            (Some(2 /* program discriminator */), true) => {
+                let offset: usize = 4 /* discriminator */;
+                Pubkey::try_from(data[offset..]).map_err(|_| ProgramError::InvalidAccountData)?
+            }
+            _ => {
+                // TODO: use custom error (invalid program state)
+                return Err(ProgramError::InvalidAccountData);
+            }
         }
     };
 
-    // program data
-    // - must match the program account data
-
-    if program_data_key != *program_data.key() {
+    // Program <-> Program Data check.
+    if expected_program_data != *program_data.key() {
         // TODO: use custom error (invalid program data account)
         return Err(ProgramError::InvalidAccountData);
     }
 
-    let program_data_account_data = unsafe { program_data.borrow_data_unchecked() };
+    // Program Data checks.
+    let is_program_authority = {
+        let data = unsafe { program_data.borrow_data_unchecked() };
+        match (data.first(), program_data.executable()) {
+            (Some(3 /* program data discriminator */), false) => {
+                // TODO(loris): Should we handle the `None` case? If so, we just need to return `false`.
+                let offset: usize = 4 /* discriminator */ + 8 /* slot */ + 1 /* option */;
+                let authority_key = Pubkey::try_from(data[offset..])
+                    .map_err(|_| ProgramError::InvalidAccountData)?;
+                authority == &authority_key
+            }
+            _ => {
+                // TODO: use custom error (invalid program state)
+                return Err(ProgramError::InvalidAccountData);
+            }
+        }
+    };
 
-    match (program_data_account_data.first(), program_data.executable()) {
-        // 3 - program data
-        (Some(3), false) => {
-            // offset = 4 (discriminator) + 8 (slot) + 1 (option)
-            let authority_key: Pubkey = program_data_account_data[13..]
-                .try_into()
-                .map_err(|_| ProgramError::InvalidAccountData)?;
-            Ok(authority == &authority_key)
-        }
-        _ => {
-            // TODO: use custom error (invalid program state)
-            Err(ProgramError::InvalidAccountData)
-        }
-    }
+    Ok(is_program_authority)
 }
 
 #[inline(always)]
