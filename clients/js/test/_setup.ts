15ecbd6754d3e05b5a5fae6c771a58cc368b315e
Loris Leiva <loris.leiva@gmail.com>
2025-01-15 15:16:29 +0100

    [wip] Add `createDeployedProgram` helper in JS tests

--- clients/js/test/_setup.ts
+++ clients/js/test/_setup.ts
@@ -1,3 +1,4 @@
+import { getCreateAccountInstruction } from '@solana-program/system';
 import {
   Address,
   Commitment,
@@ -20,7 +21,19 @@ import {
   setTransactionMessageFeePayerSigner,
   setTransactionMessageLifetimeUsingBlockhash,
   signTransactionMessageWithSigners,
+  appendTransactionMessageInstructions,
+  getAddressEncoder,
+  getProgramDerivedAddress,
+  getBase64Encoder,
+  KeyPairSigner,
 } from '@solana/web3.js';
+import { getDeployWithMaxDataLenInstruction } from './loader-v3/deploy';
+import { getWriteInstruction } from './loader-v3/write';
+import { getInitializeBufferInstruction } from './loader-v3/initializeBuffer';
+import { LOADER_V3_PROGRAM_ADDRESS } from './loader-v3/shared';
+
+const SMALLER_VALID_PROGRAM_BINARY =
+  'f0VMRgIBAQAAAAAAAAAAAAMA9wABAAAA6AAAAAAAAABAAAAAAAAAAMgBAAAAAAAAAAAAAEAAOAADAEAABgAFAAEAAAAFAAAA6AAAAAAAAADoAAAAAAAAAOgAAAAAAAAACAAAAAAAAAAIAAAAAAAAAAAQAAAAAAAAAQAAAAQAAABgAQAAAAAAAGABAAAAAAAAYAEAAAAAAAA8AAAAAAAAADwAAAAAAAAAABAAAAAAAAACAAAABgAAAPAAAAAAAAAA8AAAAAAAAADwAAAAAAAAAHAAAAAAAAAAcAAAAAAAAAAIAAAAAAAAAJUAAAAAAAAAHgAAAAAAAAAEAAAAAAAAAAYAAAAAAAAAYAEAAAAAAAALAAAAAAAAABgAAAAAAAAABQAAAAAAAACQAQAAAAAAAAoAAAAAAAAADAAAAAAAAAAWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAQAAEA6AAAAAAAAAAAAAAAAAAAAABlbnRyeXBvaW50AAAudGV4dAAuZHluYW1pYwAuZHluc3ltAC5keW5zdHIALnNoc3RydGFiAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAABAAAABgAAAAAAAADoAAAAAAAAAOgAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAHAAAABgAAAAMAAAAAAAAA8AAAAAAAAADwAAAAAAAAAHAAAAAAAAAABAAAAAAAAAAIAAAAAAAAABAAAAAAAAAAEAAAAAsAAAACAAAAAAAAAGABAAAAAAAAYAEAAAAAAAAwAAAAAAAAAAQAAAABAAAACAAAAAAAAAAYAAAAAAAAABgAAAADAAAAAgAAAAAAAACQAQAAAAAAAJABAAAAAAAADAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAgAAAAAwAAAAAAAAAAAAAAAAAAAAAAAACcAQAAAAAAACoAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAA';
 
 type Client = {
   rpc: Rpc<SolanaRpcApi>;
@@ -78,3 +91,80 @@ export const signAndSendTransaction = async (
 export const getBalance = async (client: Client, address: Address) =>
   (await client.rpc.getBalance(address, { commitment: 'confirmed' }).send())
     .value;
+
+export const createDeployedProgram = async (
+  client: Client,
+  authority: KeyPairSigner,
+  payer?: KeyPairSigner
+): Promise<[Address, Address]> => {
+  // Prepare all inputs.
+  payer = payer ?? authority;
+  const data = getBase64Encoder().encode(SMALLER_VALID_PROGRAM_BINARY);
+  const dataSize = BigInt(37 + data.length);
+  const programSize = 36n;
+  const [buffer, program, dataRent, programRent, defaultTransaction] =
+    await Promise.all([
+      generateKeyPairSigner(),
+      generateKeyPairSigner(),
+      client.rpc.getMinimumBalanceForRentExemption(dataSize).send(),
+      client.rpc.getMinimumBalanceForRentExemption(programSize).send(),
+      createDefaultTransaction(client, payer),
+    ]);
+  const [programData] = await getProgramDerivedAddress({
+    programAddress: LOADER_V3_PROGRAM_ADDRESS,
+    seeds: [getAddressEncoder().encode(program.address)],
+  });
+
+  // Create instructions.
+  const createBufferIx = getCreateAccountInstruction({
+    payer,
+    newAccount: buffer,
+    lamports: dataRent,
+    space: dataSize,
+    programAddress: LOADER_V3_PROGRAM_ADDRESS,
+  });
+  const initializeBufferIx = getInitializeBufferInstruction({
+    sourceAccount: buffer.address,
+    bufferAuthority: authority.address,
+  });
+  const writeIx = getWriteInstruction({
+    bufferAccount: buffer.address,
+    bufferAuthority: authority,
+    offset: 0,
+    bytes: data,
+  });
+  const createProgramIx = getCreateAccountInstruction({
+    payer,
+    newAccount: program,
+    lamports: programRent,
+    space: programSize,
+    programAddress: LOADER_V3_PROGRAM_ADDRESS,
+  });
+  const deployIx = getDeployWithMaxDataLenInstruction({
+    payerAccount: payer,
+    programDataAccount: programData,
+    programAccount: program.address,
+    bufferAccount: buffer.address,
+    authority,
+    maxDataLen: data.length,
+  });
+
+  // Send instructions.
+  await pipe(
+    defaultTransaction,
+    (tx) =>
+      appendTransactionMessageInstructions(
+        [createBufferIx, initializeBufferIx, writeIx],
+        tx
+      ),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+  await pipe(
+    defaultTransaction,
+    (tx) =>
+      appendTransactionMessageInstructions([createProgramIx, deployIx], tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  return [program.address, programData];
+};
---
+++ clients/js/test/loader-v3/write.ts
@@ -0,0 +1,200 @@
+import {
+  addDecoderSizePrefix,
+  addEncoderSizePrefix,
+  combineCodec,
+  getBytesDecoder,
+  getBytesEncoder,
+  getStructDecoder,
+  getStructEncoder,
+  getU32Decoder,
+  getU32Encoder,
+  getU64Decoder,
+  getU64Encoder,
+  transformEncoder,
+  type Address,
+  type Codec,
+  type Decoder,
+  type Encoder,
+  type IAccountMeta,
+  type IAccountSignerMeta,
+  type IInstruction,
+  type IInstructionWithAccounts,
+  type IInstructionWithData,
+  type ReadonlySignerAccount,
+  type ReadonlyUint8Array,
+  type TransactionSigner,
+  type WritableAccount,
+} from '@solana/web3.js';
+import {
+  LOADER_V3_PROGRAM_ADDRESS,
+  getAccountMetaFactory,
+  type ResolvedAccount,
+} from './shared';
+
+export const WRITE_DISCRIMINATOR = 1;
+
+export function getWriteDiscriminatorBytes() {
+  return getU32Encoder().encode(WRITE_DISCRIMINATOR);
+}
+
+export type WriteInstruction<
+  TProgram extends string = typeof LOADER_V3_PROGRAM_ADDRESS,
+  TAccountBufferAccount extends string | IAccountMeta<string> = string,
+  TAccountBufferAuthority extends string | IAccountMeta<string> = string,
+  TRemainingAccounts extends readonly IAccountMeta<string>[] = [],
+> = IInstruction<TProgram> &
+  IInstructionWithData<Uint8Array> &
+  IInstructionWithAccounts<
+    [
+      TAccountBufferAccount extends string
+        ? WritableAccount<TAccountBufferAccount>
+        : TAccountBufferAccount,
+      TAccountBufferAuthority extends string
+        ? ReadonlySignerAccount<TAccountBufferAuthority> &
+            IAccountSignerMeta<TAccountBufferAuthority>
+        : TAccountBufferAuthority,
+      ...TRemainingAccounts,
+    ]
+  >;
+
+export type WriteInstructionData = {
+  discriminator: number;
+  offset: number;
+  bytes: ReadonlyUint8Array;
+};
+
+export type WriteInstructionDataArgs = {
+  offset: number;
+  bytes: ReadonlyUint8Array;
+};
+
+export function getWriteInstructionDataEncoder(): Encoder<WriteInstructionDataArgs> {
+  return transformEncoder(
+    getStructEncoder([
+      ['discriminator', getU32Encoder()],
+      ['offset', getU32Encoder()],
+      ['bytes', addEncoderSizePrefix(getBytesEncoder(), getU64Encoder())],
+    ]),
+    (value) => ({ ...value, discriminator: WRITE_DISCRIMINATOR })
+  );
+}
+
+export function getWriteInstructionDataDecoder(): Decoder<WriteInstructionData> {
+  return getStructDecoder([
+    ['discriminator', getU32Decoder()],
+    ['offset', getU32Decoder()],
+    ['bytes', addDecoderSizePrefix(getBytesDecoder(), getU64Decoder())],
+  ]);
+}
+
+export function getWriteInstructionDataCodec(): Codec<
+  WriteInstructionDataArgs,
+  WriteInstructionData
+> {
+  return combineCodec(
+    getWriteInstructionDataEncoder(),
+    getWriteInstructionDataDecoder()
+  );
+}
+
+export type WriteInput<
+  TAccountBufferAccount extends string = string,
+  TAccountBufferAuthority extends string = string,
+> = {
+  /** Buffer account. */
+  bufferAccount: Address<TAccountBufferAccount>;
+  /** Buffer authority. */
+  bufferAuthority: TransactionSigner<TAccountBufferAuthority>;
+  offset: WriteInstructionDataArgs['offset'];
+  bytes: WriteInstructionDataArgs['bytes'];
+};
+
+export function getWriteInstruction<
+  TAccountBufferAccount extends string,
+  TAccountBufferAuthority extends string,
+>(
+  input: WriteInput<TAccountBufferAccount, TAccountBufferAuthority>
+): WriteInstruction<
+  typeof LOADER_V3_PROGRAM_ADDRESS,
+  TAccountBufferAccount,
+  TAccountBufferAuthority
+> {
+  // Program address.
+  const programAddress = LOADER_V3_PROGRAM_ADDRESS;
+
+  // Original accounts.
+  const originalAccounts = {
+    bufferAccount: { value: input.bufferAccount ?? null, isWritable: true },
+    bufferAuthority: {
+      value: input.bufferAuthority ?? null,
+      isWritable: false,
+    },
+  };
+  const accounts = originalAccounts as Record<
+    keyof typeof originalAccounts,
+    ResolvedAccount
+  >;
+
+  // Original args.
+  const args = { ...input };
+
+  const getAccountMeta = getAccountMetaFactory(programAddress, 'programId');
+  const instruction = {
+    accounts: [
+      getAccountMeta(accounts.bufferAccount),
+      getAccountMeta(accounts.bufferAuthority),
+    ],
+    programAddress,
+    data: getWriteInstructionDataEncoder().encode(
+      args as WriteInstructionDataArgs
+    ),
+  } as WriteInstruction<
+    typeof LOADER_V3_PROGRAM_ADDRESS,
+    TAccountBufferAccount,
+    TAccountBufferAuthority
+  >;
+
+  return instruction;
+}
+
+export type ParsedWriteInstruction<
+  TProgram extends string = typeof LOADER_V3_PROGRAM_ADDRESS,
+  TAccountMetas extends readonly IAccountMeta[] = readonly IAccountMeta[],
+> = {
+  programAddress: Address<TProgram>;
+  accounts: {
+    /** Buffer account. */
+    bufferAccount: TAccountMetas[0];
+    /** Buffer authority. */
+    bufferAuthority: TAccountMetas[1];
+  };
+  data: WriteInstructionData;
+};
+
+export function parseWriteInstruction<
+  TProgram extends string,
+  TAccountMetas extends readonly IAccountMeta[],
+>(
+  instruction: IInstruction<TProgram> &
+    IInstructionWithAccounts<TAccountMetas> &
+    IInstructionWithData<Uint8Array>
+): ParsedWriteInstruction<TProgram, TAccountMetas> {
+  if (instruction.accounts.length < 2) {
+    // TODO: Coded error.
+    throw new Error('Not enough accounts');
+  }
+  let accountIndex = 0;
+  const getNextAccount = () => {
+    const accountMeta = instruction.accounts![accountIndex]!;
+    accountIndex += 1;
+    return accountMeta;
+  };
+  return {
+    programAddress: instruction.programAddress,
+    accounts: {
+      bufferAccount: getNextAccount(),
+      bufferAuthority: getNextAccount(),
+    },
+    data: getWriteInstructionDataDecoder().decode(instruction.data),
+  };
+}
