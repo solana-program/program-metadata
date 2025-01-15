cade8c3e414744381bb9550da2fae384c116c33a
Loris Leiva <loris.leiva@gmail.com>
2025-01-15 16:08:20 +0100

    [wip]: Add initialize test for canonical PDAs

--- clients/js/test/initialize.test.ts
+++ clients/js/test/initialize.test.ts
@@ -13,6 +13,7 @@ import {
   DataSource,
   Encoding,
   fetchMetadata,
+  findCanonicalPda,
   findNonCanonicalPda,
   Format,
   getExternalDataEncoder,
@@ -22,9 +23,67 @@ import {
 import {
   createDefaultSolanaClient,
   createDefaultTransaction,
+  createDeployedProgram,
   generateKeyPairSignerWithSol,
   signAndSendTransaction,
 } from './_setup';
+
+test('it initializes a canonical PDA with direct data from instruction data', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const authority = await generateKeyPairSignerWithSol(client);
+  const [program, programData] = await createDeployedProgram(client, authority);
+
+  // And the following metadata seed and data.
+  const seed = 'dummy';
+  const data = getUtf8Encoder().encode('Hello, World!');
+
+  // And given the metadata account is pre-funded.
+  const [metadata] = await findCanonicalPda({ program, seed });
+  const rent = await client.rpc
+    .getMinimumBalanceForRentExemption(96n + BigInt(data.length))
+    .send();
+  const preFund = getTransferSolInstruction({
+    source: authority,
+    destination: metadata,
+    amount: rent,
+  });
+
+  // When we initialize the metadata account with the data on the instruction.
+  const initialize = await getInitializeInstructionAsync({
+    authority,
+    program,
+    programData,
+    seed,
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    format: Format.None,
+    dataSource: DataSource.Direct,
+    data,
+  });
+  await pipe(
+    await createDefaultTransaction(client, authority),
+    (tx) => appendTransactionMessageInstructions([preFund, initialize], tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we expect the following metadata account to be created.
+  const metadataAccount = await fetchMetadata(client.rpc, metadata);
+  t.like(metadataAccount.data, <Metadata>{
+    discriminator: AccountDiscriminator.Metadata,
+    program,
+    authority: some(authority.address),
+    mutable: true,
+    canonical: true,
+    seed: 'dummy',
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    format: Format.None,
+    dataSource: DataSource.Direct,
+    dataLength: data.length,
+    data,
+  });
+});
 
 test('it initializes a non canonical PDA with direct data from instruction data', async (t) => {
   // Given the following authority and program.
