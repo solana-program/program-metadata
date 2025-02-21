afe3f97810d1a43769e219ef5a5bab4e8b9a1691
Loris Leiva <loris.leiva@gmail.com>
2025-02-21 16:58:53 -0700

    Add JS tests for extend instruction

--- clients/js/test/extend.test.ts
+++ clients/js/test/extend.test.ts
@@ -1,13 +1,182 @@
+import { getTransferSolInstruction } from '@solana-program/system';
+import {
+  address,
+  appendTransactionMessageInstructions,
+  assertAccountExists,
+  BASE_ACCOUNT_SIZE,
+  fetchEncodedAccount,
+  generateKeyPairSigner,
+  getUtf8Encoder,
+  lamports,
+  pipe,
+} from '@solana/web3.js';
 import test from 'ava';
+import {
+  ACCOUNT_HEADER_LENGTH,
+  getExtendInstruction,
+  getSetAuthorityInstruction,
+} from '../src';
+import {
+  createCanonicalMetadata,
+  createDefaultSolanaClient,
+  createDefaultTransaction,
+  createDeployedProgram,
+  createNonCanonicalMetadata,
+  generateKeyPairSignerWithSol,
+  signAndSendTransaction,
+} from './_setup';
 
-test.todo(
-  'the program authority of a canonical metadata account can extend it'
-);
+test('the program authority of a canonical metadata account can extend it', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const authority = await generateKeyPairSignerWithSol(client);
+  const [program, programData] = await createDeployedProgram(client, authority);
 
-test.todo(
-  'the explicit authority of a canonical metadata account can extend it'
-);
+  // And the following metadata account with 200 bytes of data.
+  const data = getUtf8Encoder().encode('x'.repeat(200));
+  const [metadata] = await createCanonicalMetadata(client, {
+    authority,
+    data,
+    program,
+    programData,
+    seed: 'dummy',
+  });
 
-test.todo(
-  'the metadata authority of a non-canonical metadata account can extend it'
-);
+  // When we extend the metadata account by 100 bytes.
+  const rentForHeader = await client.rpc
+    .getMinimumBalanceForRentExemption(0n)
+    .send();
+  const extraRent = lamports(
+    (rentForHeader * 100n) / BigInt(BASE_ACCOUNT_SIZE)
+  );
+  const transferIx = getTransferSolInstruction({
+    source: authority,
+    destination: metadata,
+    amount: extraRent,
+  });
+  const extendIx = getExtendInstruction({
+    account: metadata,
+    authority,
+    length: 100,
+    program,
+    programData,
+  });
+  await pipe(
+    await createDefaultTransaction(client, authority),
+    (tx) => appendTransactionMessageInstructions([transferIx, extendIx], tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we expect the metadata account to be extended.
+  const metadataAccount = await fetchEncodedAccount(client.rpc, metadata);
+  assertAccountExists(metadataAccount);
+  t.is(metadataAccount.data.length, ACCOUNT_HEADER_LENGTH + 300);
+});
+
+test('the explicit authority of a canonical metadata account can extend it', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const [programAuthority, authority] = await Promise.all([
+    generateKeyPairSignerWithSol(client),
+    generateKeyPairSigner(),
+  ]);
+  const [program, programData] = await createDeployedProgram(
+    client,
+    programAuthority
+  );
+
+  // And the following metadata account with 200 bytes of data.
+  const data = getUtf8Encoder().encode('x'.repeat(200));
+  const [metadata] = await createCanonicalMetadata(client, {
+    authority: programAuthority,
+    data,
+    program,
+    programData,
+    seed: 'dummy',
+  });
+
+  // And an explicit authority is set for the metadata account.
+  const setAuthorityIx = getSetAuthorityInstruction({
+    account: metadata,
+    authority: programAuthority,
+    newAuthority: authority.address,
+    program,
+    programData,
+  });
+
+  // When we extend the metadata account by 100 bytes.
+  const rentForHeader = await client.rpc
+    .getMinimumBalanceForRentExemption(0n)
+    .send();
+  const extraRent = lamports(
+    (rentForHeader * 100n) / BigInt(BASE_ACCOUNT_SIZE)
+  );
+  const transferIx = getTransferSolInstruction({
+    source: programAuthority,
+    destination: metadata,
+    amount: extraRent,
+  });
+  const extendIx = getExtendInstruction({
+    account: metadata,
+    authority,
+    length: 100,
+  });
+  await pipe(
+    await createDefaultTransaction(client, programAuthority),
+    (tx) =>
+      appendTransactionMessageInstructions(
+        [setAuthorityIx, transferIx, extendIx],
+        tx
+      ),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we expect the metadata account to be extended.
+  const metadataAccount = await fetchEncodedAccount(client.rpc, metadata);
+  assertAccountExists(metadataAccount);
+  t.is(metadataAccount.data.length, ACCOUNT_HEADER_LENGTH + 300);
+});
+
+test('the metadata authority of a non-canonical metadata account can extend it', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const authority = await generateKeyPairSignerWithSol(client);
+  const program = address('TokenKEGQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+  // And the following metadata account with 200 bytes of data.
+  const data = getUtf8Encoder().encode('x'.repeat(200));
+  const [metadata] = await createNonCanonicalMetadata(client, {
+    authority,
+    data,
+    program,
+    seed: 'dummy',
+  });
+
+  // When we extend the metadata account by 100 bytes.
+  const rentForHeader = await client.rpc
+    .getMinimumBalanceForRentExemption(0n)
+    .send();
+  const extraRent = lamports(
+    (rentForHeader * 100n) / BigInt(BASE_ACCOUNT_SIZE)
+  );
+  const transferIx = getTransferSolInstruction({
+    source: authority,
+    destination: metadata,
+    amount: extraRent,
+  });
+  const extendIx = getExtendInstruction({
+    account: metadata,
+    authority,
+    length: 100,
+  });
+  await pipe(
+    await createDefaultTransaction(client, authority),
+    (tx) => appendTransactionMessageInstructions([transferIx, extendIx], tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we expect the metadata account to be extended.
+  const metadataAccount = await fetchEncodedAccount(client.rpc, metadata);
+  assertAccountExists(metadataAccount);
+  t.is(metadataAccount.data.length, ACCOUNT_HEADER_LENGTH + 300);
+});
--- clients/js/test/trim.test.ts
+++ clients/js/test/trim.test.ts
@@ -1,6 +1,97 @@
+import {
+  appendTransactionMessageInstructions,
+  BASE_ACCOUNT_SIZE,
+  generateKeyPairSigner,
+  getUtf8Encoder,
+  lamports,
+  pipe,
+} from '@solana/web3.js';
 import test from 'ava';
+import {
+  AccountDiscriminator,
+  Compression,
+  DataSource,
+  Encoding,
+  fetchMetadata,
+  Format,
+  getSetDataInstruction,
+  getTrimInstruction,
+  Metadata,
+} from '../src';
+import {
+  createCanonicalMetadata,
+  createDefaultSolanaClient,
+  createDefaultTransaction,
+  createDeployedProgram,
+  generateKeyPairSignerWithSol,
+  getBalance,
+  signAndSendTransaction,
+} from './_setup';
 
-test.todo('the program authority of a canonical metadata account can trim it');
+test.skip('the program authority of a canonical metadata account can trim it', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const [authority, destination] = await Promise.all([
+    generateKeyPairSignerWithSol(client),
+    generateKeyPairSigner(),
+  ]);
+  const [program, programData] = await createDeployedProgram(client, authority);
+
+  // And the following metadata account with 200 bytes of data.
+  const data = getUtf8Encoder().encode('x'.repeat(200));
+  const [metadata] = await createCanonicalMetadata(client, {
+    authority,
+    data,
+    program,
+    programData,
+    seed: 'dummy',
+  });
+
+  // And given we remove 100 bytes of data.
+  const reducedData = getUtf8Encoder().encode('x'.repeat(100));
+  const reduceBytesIx = getSetDataInstruction({
+    metadata,
+    authority,
+    encoding: Encoding.None,
+    compression: Compression.None,
+    format: Format.None,
+    dataSource: DataSource.Direct,
+    data: reducedData,
+    program,
+    programData,
+  });
+
+  // When we trim the metadata account.
+  const trimIx = getTrimInstruction({
+    account: metadata,
+    authority,
+    destination: destination.address,
+    program,
+    programData,
+  });
+  await pipe(
+    await createDefaultTransaction(client, authority),
+    (tx) => appendTransactionMessageInstructions([reduceBytesIx, trimIx], tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we expect the metadata account to be trimmed.
+  const metadataAccount = await fetchMetadata(client.rpc, metadata);
+  t.like(metadataAccount.data, <Metadata>{
+    discriminator: AccountDiscriminator.Metadata,
+    data: reducedData,
+  });
+
+  // And we expect the destination account to have the rent difference.
+  const rentForHeader = await client.rpc
+    .getMinimumBalanceForRentExemption(0n)
+    .send();
+  const rentDifference = lamports(
+    (rentForHeader * (200n - 100n)) / BigInt(BASE_ACCOUNT_SIZE)
+  );
+  const destinationBalance = await getBalance(client, destination.address);
+  t.is(destinationBalance, rentDifference);
+});
 
 test.todo('the explicit authority of a canonical metadata account can trim it');
 
