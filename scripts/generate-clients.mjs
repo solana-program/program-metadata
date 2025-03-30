e865ab41f749ab747cd92c4a9cdda42c834f495f
febo <febo@anza.xyz>
2025-03-30 02:14:56 +0100

    Update scripts

--- scripts/generate-clients.mjs
+++ scripts/generate-clients.mjs
@@ -1,106 +1,28 @@
 #!/usr/bin/env zx
 import 'zx/globals';
-import * as c from 'codama';
+import { createFromRoot } from 'codama';
 import { renderVisitor as renderJavaScriptVisitor } from '@codama/renderers-js';
-import { renderVisitor as renderRustVisitor } from '@codama/renderers-rust';
-import { getAllProgramIdls } from './utils.mjs';
+// import { renderVisitor as renderRustVisitor } from "@codama/renderers-rust";
+import { workingDirectory } from './utils.mjs';
 
 // Instanciate Codama.
-const [idl] = getAllProgramIdls();
-const codama = c.createFromRoot(require(idl));
+const codama = createFromRoot(
+  require(path.join(workingDirectory, 'program', 'idl.json'))
+);
 
 // Render JavaScript.
 const jsClient = path.join(__dirname, '..', 'clients', 'js');
 codama.accept(
   renderJavaScriptVisitor(path.join(jsClient, 'src', 'generated'), {
-    prettierOptions: require(path.join(jsClient, '.prettierrc.json')),
+    prettier: require(path.join(jsClient, '.prettierrc.json')),
   })
-);
-
-// [Rust] Extract remainder and zeroable types so they can be hooked.
-codama.update(
-  c.bottomUpTransformerVisitor([
-    {
-      select: '[remainderOptionTypeNode]',
-      transform: (node) => {
-        c.assertIsNode(node, 'remainderOptionTypeNode');
-        if (c.isNode(node.item, 'bytesTypeNode')) {
-          return c.definedTypeLinkNode('remainderOptionBytes');
-        }
-        if (c.isNode(node.item, 'publicKeyTypeNode')) {
-          return c.definedTypeLinkNode('remainderOptionPubkey');
-        }
-        if (
-          c.isNode(node.item, 'definedTypeLinkNode') &&
-          node.item.name === 'seed'
-        ) {
-          return c.definedTypeLinkNode('remainderOptionSeed');
-        }
-        return node;
-      },
-    },
-    {
-      select: '[zeroableOptionTypeNode]',
-      transform: (node) => {
-        c.assertIsNode(node, 'zeroableOptionTypeNode');
-        if (c.isNode(node.item, 'publicKeyTypeNode')) {
-          return c.definedTypeLinkNode('zeroableOptionPubkey');
-        }
-        if (
-          c.isNode(node.item, 'numberTypeNode') &&
-          node.item.format === 'u32'
-        ) {
-          return c.definedTypeLinkNode('zeroableOptionOffset');
-        }
-        return node;
-      },
-    },
-    {
-      select: '[instructionArgumentNode]',
-      transform: (node) => {
-        c.assertIsNode(node, 'instructionArgumentNode');
-        if (node.defaultValue === undefined) {
-          return node;
-        }
-        if (!c.isNode(node.type, 'definedTypeLinkNode')) {
-          return node;
-        }
-        if (
-          !(
-            node.type.name.startsWith('remainderOption') ||
-            node.type.name.startsWith('zeroableOption')
-          )
-        ) {
-          return node;
-        }
-        return c.instructionArgumentNode({ ...node, defaultValue: undefined });
-      },
-    },
-    {
-      select: '[accountNode]',
-      transform: (node) => {
-        c.assertIsNode(node, 'accountNode');
-        return c.accountNode({ ...node, pda: undefined });
-      },
-    },
-  ])
 );
 
 // Render Rust.
-const rustClient = path.join(__dirname, '..', 'clients', 'rust');
-codama.accept(
-  renderRustVisitor(path.join(rustClient, 'src', 'generated'), {
-    formatCode: true,
-    crateFolder: rustClient,
-    anchorTraits: false,
-    linkOverrides: {
-      definedTypes: {
-        remainderOptionBytes: 'hooked',
-        remainderOptionPubkey: 'hooked',
-        remainderOptionSeed: 'hooked',
-        zeroableOptionPubkey: 'hooked',
-        zeroableOptionOffset: 'hooked',
-      },
-    },
-  })
-);
+// const rustClient = path.join(__dirname, "..", "clients", "rust");
+// codama.accept(
+//   renderRustVisitor(path.join(rustClient, "src", "generated"), {
+//     formatCode: true,
+//     crateFolder: rustClient,
+//   })
+// );
---
+++ scripts/rust/test.mjs
@@ -0,0 +1,8 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+import { cliArguments, workingDirectory } from '../utils.mjs';
+
+const [folder, ...args] = cliArguments();
+const sbfOutDir = path.join(workingDirectory, 'target', 'deploy');
+const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
+await $`SBF_OUT_DIR=${sbfOutDir} cargo test --manifest-path ${manifestPath} ${args}`;
