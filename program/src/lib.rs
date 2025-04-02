a4a1c5c1dc6f6ac732f1b28d6bee6926a90d8b12
febo <febo@anza.xyz>
2025-04-02 11:21:48 +0100

    Use security.txt macro

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -9,3 +9,15 @@ pub mod processor;
 pub mod state;
 
 pinocchio_pubkey::declare_id!("ProgM6JCCvbYkfKqJYHePx4xxSUSqJp7rh8Lyv7nk7S");
+
+solana_security_txt::security_txt! {
+    // Required fields
+    name: "SPL Program Metadata",
+    project_url: "https://github.com/solana-program/program-metadata",
+    contacts: "link:https://github.com/solana-program/program-metadata/security/advisories/new,email:security@anza.xyz,link:https://solana.com/discord",
+    policy: "https://github.com/solana-program/program-metadata/blob/master/SECURITY.md",
+
+    // Optional Fields
+    preferred_languages: "en",
+    source_code: "https://github.com/solana-program/program-metadata/tree/master/program"
+}
