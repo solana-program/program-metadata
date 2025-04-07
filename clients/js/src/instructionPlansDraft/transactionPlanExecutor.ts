4a034d1424c4ac21c0ba7298b3dbcd021be81d9f
Loris Leiva <loris.leiva@gmail.com>
2025-04-07 14:00:21 +0100

    wip

--- clients/js/src/instructionPlansDraft/transactionPlanExecutor.ts
+++ clients/js/src/instructionPlansDraft/transactionPlanExecutor.ts
@@ -1,8 +1,5 @@
 import {
-  Blockhash,
   GetLatestBlockhashApi,
-  isSolanaError,
-  pipe,
   Rpc,
   RpcSubscriptions,
   Signature,
@@ -15,12 +12,10 @@ export type TransactionPlanExecutor<TContext extends object | null = null> = (
   transactionPlan: TransactionPlan
 ) => Promise<TransactionPlanResult<TContext>>;
 
-export function getDefaultTransactionPlanExecutor(options: {
+export function createBaseTransactionPlanExecutor(options: {
   rpc: Rpc<GetLatestBlockhashApi>;
-  rpcSubscriptions: RpcSubscriptions<SolanaRpcSubscriptionsApi>; // TODO: narrow
+  rpcSubscriptions: RpcSubscriptions<SolanaRpcSubscriptionsApi>;
 }): TransactionPlanExecutor {
-  const { rpc } = options;
-
   // TODO: implement
   // - Refetch blockhash if it's expired
   // - Retry on failure
@@ -38,65 +33,5 @@ export function getDefaultTransactionPlanExecutor(options: {
     };
   };
 
-  return pipe(
-    executor,
-    (ex) => refreshBlockheightTransactionPlanExecutor(rpc, ex),
-    (ex) => retryTransactionPlanExecutor(5, ex)
-  );
-}
-
-export function refreshBlockheightTransactionPlanExecutor(
-  rpc: Rpc<GetLatestBlockhashApi>,
-  executor: TransactionPlanExecutor
-): TransactionPlanExecutor {
-  let latestBlockhash: {
-    blockhash: Blockhash;
-    lastValidBlockHeight: bigint;
-  } | null = null;
-  return async (transactionPlan) => {
-    if (transactionPlan.kind !== 'single') {
-      return await executor(transactionPlan);
-    }
-
-    if (latestBlockhash) {
-      // Replace the blockhash in the message
-    }
-    try {
-      return await executor(transactionPlan);
-    } catch (error) {
-      if (isSolanaError(error)) {
-        // TODO: Retry on blockhash expired error
-        const result = await rpc.getLatestBlockhash().send();
-        latestBlockhash = result.value;
-        return await executor(transactionPlan);
-      } else {
-        throw error;
-      }
-    }
-  };
-}
-
-export function retryTransactionPlanExecutor(
-  maxRetries: number,
-  executor: TransactionPlanExecutor
-): TransactionPlanExecutor {
-  return async (transactionPlan) => {
-    if (transactionPlan.kind !== 'single') {
-      return await executor(transactionPlan);
-    }
-
-    let retries = 0;
-    let lastError: Error | null = null;
-
-    while (retries < maxRetries) {
-      try {
-        return await executor(transactionPlan);
-      } catch (error) {
-        retries++;
-        lastError = error as Error;
-      }
-    }
-
-    throw lastError;
-  };
+  return executor;
 }
---
+++ clients/js/src/instructionPlansDraft/transactionPlanExecutorDecorators.ts
@@ -0,0 +1,81 @@
+import {
+  Blockhash,
+  GetLatestBlockhashApi,
+  isSolanaError,
+  Rpc,
+  setTransactionMessageLifetimeUsingBlockhash,
+  SOLANA_ERROR__TRANSACTION_ERROR__BLOCKHASH_NOT_FOUND,
+} from '@solana/kit';
+import { SingleTransactionPlan } from './transactionPlan';
+import { TransactionPlanExecutor } from './transactionPlanExecutor';
+
+type Mutable<T> = { -readonly [P in keyof T]: T[P] };
+
+export function refreshBlockheightTransactionPlanExecutor(
+  rpc: Rpc<GetLatestBlockhashApi>,
+  executor: TransactionPlanExecutor
+): TransactionPlanExecutor {
+  let latestBlockhash: {
+    blockhash: Blockhash;
+    lastValidBlockHeight: bigint;
+  } | null = null;
+  return async function traverse(transactionPlan) {
+    if (transactionPlan.kind !== 'single') {
+      return await executor(transactionPlan);
+    }
+
+    // Replace the blockhash in the message, if a new one is available.
+    if (latestBlockhash) {
+      (transactionPlan as Mutable<SingleTransactionPlan>).message =
+        setTransactionMessageLifetimeUsingBlockhash(
+          latestBlockhash,
+          transactionPlan.message
+        );
+    }
+
+    try {
+      return await executor(transactionPlan);
+    } catch (error) {
+      if (
+        isSolanaError(
+          error,
+          // TODO: Retry on blockhash expired error.
+          SOLANA_ERROR__TRANSACTION_ERROR__BLOCKHASH_NOT_FOUND
+        )
+      ) {
+        const result = await rpc.getLatestBlockhash().send();
+        latestBlockhash = result.value;
+        return await traverse(transactionPlan);
+      } else {
+        throw error;
+      }
+    }
+  };
+}
+
+export function retryTransactionPlanExecutor(
+  maxRetries: number,
+  executor: TransactionPlanExecutor
+): TransactionPlanExecutor {
+  return async (transactionPlan) => {
+    if (transactionPlan.kind !== 'single') {
+      return await executor(transactionPlan);
+    }
+
+    let retries = 0;
+    let lastError: Error | null = null;
+
+    // x retries means x+1 attempts.
+    while (retries < maxRetries + 1) {
+      try {
+        return await executor(transactionPlan);
+      } catch (error) {
+        // TODO: Should we not retry on certain error codes?
+        retries++;
+        lastError = error as Error;
+      }
+    }
+
+    throw lastError;
+  };
+}
