f1de283b830c1974a274062c4d8d1296e33ae851
Loris Leiva <loris.leiva@gmail.com>
2025-04-07 13:47:05 +0100

    wip

--- clients/js/src/instructionPlansDraft/transactionPlannerDecorators.ts
+++ clients/js/src/instructionPlansDraft/transactionPlannerDecorators.ts
@@ -2,11 +2,13 @@ import {
   Address,
   appendTransactionMessageInstructions,
   BaseTransactionMessage,
+  CompilableTransactionMessage,
   getComputeUnitEstimateForTransactionMessageFactory,
   GetLatestBlockhashApi,
   IInstruction,
   ITransactionMessageWithFeePayer,
   ITransactionMessageWithFeePayerSigner,
+  prependTransactionMessageInstruction,
   prependTransactionMessageInstructions,
   Rpc,
   setTransactionMessageFeePayer,
@@ -25,8 +27,11 @@ import {
   getSetComputeUnitLimitInstruction,
   identifyComputeBudgetInstruction,
 } from '@solana-program/compute-budget';
+import { SingleTransactionPlan, TransactionPlan } from './transactionPlan';
 
-export function transformTransactionPlannerMessage(
+type Mutable<T> = { -readonly [P in keyof T]: T[P] };
+
+export function transformNewTransactionPlannerMessage(
   transformer: Required<TransactionPlannerConfig>['newTransactionTransformer'],
   planner: TransactionPlanner
 ): TransactionPlanner {
@@ -43,11 +48,20 @@ export function transformTransactionPlannerMessage(
   };
 }
 
+export function transformTransactionPlan(
+  transformer: (transactionPlan: TransactionPlan) => Promise<TransactionPlan>,
+  planner: TransactionPlanner
+): TransactionPlanner {
+  return async (instructionPlan, config) => {
+    return await transformer(await planner(instructionPlan, config));
+  };
+}
+
 export function prependTransactionPlannerInstructions(
   instructions: IInstruction[],
   planner: TransactionPlanner
 ): TransactionPlanner {
-  return transformTransactionPlannerMessage(
+  return transformNewTransactionPlannerMessage(
     (tx) =>
       Promise.resolve(prependTransactionMessageInstructions(instructions, tx)),
     planner
@@ -58,7 +72,7 @@ export function appendTransactionPlannerInstructions(
   instructions: IInstruction[],
   planner: TransactionPlanner
 ): TransactionPlanner {
-  return transformTransactionPlannerMessage(
+  return transformNewTransactionPlannerMessage(
     (tx) =>
       Promise.resolve(appendTransactionMessageInstructions(instructions, tx)),
     planner
@@ -69,7 +83,7 @@ export function setTransactionPlannerFeePayer(
   feePayer: Address,
   planner: TransactionPlanner
 ): TransactionPlanner {
-  return transformTransactionPlannerMessage(
+  return transformNewTransactionPlannerMessage(
     <TTransactionMessage extends BaseTransactionMessage>(
       tx: TTransactionMessage
     ) =>
@@ -85,7 +99,7 @@ export function setTransactionPlannerFeePayerSigner(
   feePayerSigner: TransactionSigner,
   planner: TransactionPlanner
 ): TransactionPlanner {
-  return transformTransactionPlannerMessage(
+  return transformNewTransactionPlannerMessage(
     <TTransactionMessage extends BaseTransactionMessage>(
       tx: TTransactionMessage
     ) =>
@@ -109,7 +123,7 @@ export function setTransactionPlannerLifetimeUsingLatestBlockhash(
     return value;
   }, 60_000);
 
-  return transformTransactionPlannerMessage(
+  return transformNewTransactionPlannerMessage(
     async (tx) =>
       setTransactionMessageLifetimeUsingBlockhash(await getBlockhash(), tx),
     planner
@@ -124,30 +138,80 @@ const MAX_COMPUTE_UNIT_LIMIT = 1_400_000;
 // move in a granular package so `instruction-plans` can use it.
 export function estimateAndSetComputeUnitLimitForTransactionPlanner(
   rpc: Rpc<SimulateTransactionApi>,
-  planner: TransactionPlanner
+  planner: TransactionPlanner,
+  chunkSize: number | null = 10
 ): TransactionPlanner {
-  const estimate = getComputeUnitEstimateForTransactionMessageFactory({ rpc });
+  // Create a function to estimate the compute unit limit for a transaction.
+  const estimateComputeUnitLimit =
+    getComputeUnitEstimateForTransactionMessageFactory({ rpc });
 
-  return transformTransactionPlannerMessage((tx) => {
-    const hasComputeBudgetLimit = tx.instructions.some((ix) => {
-      return (
-        ix.programAddress === COMPUTE_BUDGET_PROGRAM_ADDRESS &&
-        identifyComputeBudgetInstruction(ix.data as Uint8Array) ===
-          ComputeBudgetInstruction.SetComputeUnitLimit
+  // Add a compute unit limit instruction to the transaction if it doesn't exist.
+  const plannerWithComputeBudgetLimits = transformNewTransactionPlannerMessage(
+    (tx) => {
+      if (getComputeUnitLimitInstructionIndex(tx) >= 0) {
+        return Promise.resolve(tx);
+      }
+
+      return Promise.resolve(
+        prependTransactionMessageInstruction(
+          getSetComputeUnitLimitInstruction({ units: MAX_COMPUTE_UNIT_LIMIT }),
+          tx
+        )
       );
-    });
+    },
+    planner
+  );
 
-    if (hasComputeBudgetLimit) {
-      return Promise.resolve(tx);
+  // Transform the final transaction plan to set the correct compute unit limit.
+  return transformTransactionPlan(async (plan) => {
+    const promises = getAllSingleTransactionPlans(plan).map(
+      async (singlePlan) => {
+        const computeUnitsEstimate = await estimateComputeUnitLimit(
+          singlePlan.message as CompilableTransactionMessage
+        );
+        const instructionIndex = getComputeUnitLimitInstructionIndex(
+          singlePlan.message
+        );
+        const newMessage =
+          instructionIndex === -1
+            ? prependTransactionMessageInstruction(
+                getSetComputeUnitLimitInstruction({
+                  units: computeUnitsEstimate,
+                }),
+                singlePlan.message
+              )
+            : {
+                ...singlePlan.message,
+                instructions: [
+                  ...singlePlan.message.instructions.slice(0, instructionIndex),
+                  getSetComputeUnitLimitInstruction({
+                    units: computeUnitsEstimate,
+                  }),
+                  ...singlePlan.message.instructions.slice(
+                    instructionIndex + 1
+                  ),
+                ],
+              };
+        (singlePlan as Mutable<SingleTransactionPlan>).message = newMessage;
+      }
+    );
+
+    // Chunk promises to avoid rate limiting.
+    const chunkedPromises = [];
+    if (!chunkSize) {
+      chunkedPromises.push(promises);
+    } else {
+      for (let i = 0; i < promises.length; i += chunkSize) {
+        const chunk = promises.slice(i, i + chunkSize);
+        chunkedPromises.push(chunk);
+      }
+    }
+    for (const chunk of chunkedPromises) {
+      await Promise.all(chunk);
     }
 
-    return Promise.resolve(
-      prependTransactionMessageInstructions(
-        [getSetComputeUnitLimitInstruction({ units: MAX_COMPUTE_UNIT_LIMIT })],
-        tx
-      )
-    );
-  }, planner);
+    return plan;
+  }, plannerWithComputeBudgetLimits);
 }
 
 function getTimedCacheFunction<T>(
@@ -169,4 +233,25 @@ function getTimedCacheFunction<T>(
     lastFetchTime = currentTime;
     return cache;
   };
+}
+
+function getComputeUnitLimitInstructionIndex(
+  transactionMessage: BaseTransactionMessage
+) {
+  return transactionMessage.instructions.findIndex((ix) => {
+    return (
+      ix.programAddress === COMPUTE_BUDGET_PROGRAM_ADDRESS &&
+      identifyComputeBudgetInstruction(ix.data as Uint8Array) ===
+        ComputeBudgetInstruction.SetComputeUnitLimit
+    );
+  });
+}
+
+function getAllSingleTransactionPlans(
+  transactionPlan: TransactionPlan
+): SingleTransactionPlan[] {
+  if (transactionPlan.kind === 'single') {
+    return [transactionPlan];
+  }
+  return transactionPlan.plans.flatMap(getAllSingleTransactionPlans);
 }
