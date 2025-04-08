94af55219ae5a15ac9926b2be736f37cb2dfdedd
Loris Leiva <loris.leiva@gmail.com>
2025-04-08 15:57:45 +0100

    wip

--- clients/js/src/instructionPlansDraft/transactionPlanExecutorFactoryDecorators.ts
+++ clients/js/src/instructionPlansDraft/transactionPlanExecutorFactoryDecorators.ts
@@ -5,59 +5,87 @@ import {
 } from '@solana/kit';
 import { getTimedCacheFunction, Mutable } from './internal';
 import { SingleTransactionPlan } from './transactionPlan';
-import { TransactionPlanExecutor } from './transactionPlanExecutor';
+import {
+  TransactionPlanExecutorFactory,
+  TransactionPlanExecutorFactoryConfig,
+} from './transactionPlanExecutorFactory';
 
 // TODO: implement
 // - Chunk parallel transactions (Needs special transformer)
 // - Add support for custom <TContext>
 
+export function transformTransactionPlanExecutorFactory(
+  transformer: Required<TransactionPlanExecutorFactoryConfig>['transformer'],
+  executorFactory: TransactionPlanExecutorFactory
+): TransactionPlanExecutorFactory {
+  return (config) => {
+    return executorFactory({
+      ...config,
+      transformer: (transactionPlan, next) => {
+        const newNext: typeof next = (plan) => transformer(plan, next);
+        return config?.transformer
+          ? config.transformer(transactionPlan, newNext)
+          : newNext(transactionPlan);
+      },
+    });
+  };
+}
+
 export function refreshBlockhashForTransactionPlanExecutor(
   rpc: Rpc<GetLatestBlockhashApi>,
-  executor: TransactionPlanExecutor
-): TransactionPlanExecutor {
+  executorFactory: TransactionPlanExecutorFactory
+): TransactionPlanExecutorFactory {
   // Cache the latest blockhash for 60 seconds.
   const getBlockhash = getTimedCacheFunction(async () => {
     const { value } = await rpc.getLatestBlockhash().send();
     return value;
   }, 60_000);
 
-  return async (transactionPlan) => {
-    if (transactionPlan.kind !== 'single') {
-      return await executor(transactionPlan);
-    }
+  return transformTransactionPlanExecutorFactory(
+    async (transactionPlan, next) => {
+      if (transactionPlan.kind !== 'single') {
+        return await next(transactionPlan);
+      }
 
-    (transactionPlan as Mutable<SingleTransactionPlan>).message =
-      setTransactionMessageLifetimeUsingBlockhash(
-        await getBlockhash(),
-        transactionPlan.message
-      );
-    return await executor(transactionPlan);
-  };
+      (transactionPlan as Mutable<SingleTransactionPlan>).message =
+        setTransactionMessageLifetimeUsingBlockhash(
+          await getBlockhash(),
+          transactionPlan.message
+        );
+      return await next(transactionPlan);
+    },
+    executorFactory
+  );
 }
 
 export function retryTransactionPlanExecutor(
   maxRetries: number,
-  executor: TransactionPlanExecutor
-): TransactionPlanExecutor {
-  return async (transactionPlan) => {
-    if (transactionPlan.kind !== 'single') {
-      return await executor(transactionPlan);
-    }
-
-    let retries = 0;
-    let lastError: Error | null = null;
-
-    // x retries means x+1 attempts.
-    while (retries < maxRetries + 1) {
-      try {
-        return await executor(transactionPlan);
-      } catch (error) {
-        // TODO: Should we not retry on certain error codes?
-        retries++;
-        lastError = error as Error;
+  executorFactory: TransactionPlanExecutorFactory
+): TransactionPlanExecutorFactory {
+  return transformTransactionPlanExecutorFactory(
+    async (transactionPlan, next) => {
+      if (transactionPlan.kind !== 'single') {
+        return await next(transactionPlan);
       }
-    }
 
-    throw lastError;
-  };
+      let retries = 0;
+      let lastError: Error | null = null;
+
+      // x retries means x+1 attempts.
+      while (retries < maxRetries + 1) {
+        try {
+          return await next(transactionPlan);
+        } catch (error) {
+          // TODO: Should we not retry on certain error codes?
+          retries++;
+          lastError = error as Error;
+        }
+      }
+
+      throw lastError;
+      // TODO: Catch and return failed results instead of failing.
+      // TODO: Have another decorator that fails whilst returning the result in the context at the very end.
+    },
+    executorFactory
+  );
 }
