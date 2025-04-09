c6fcd4287a6faebbf33f7ee81b2aa1481e1410a0
Loris Leiva <loris.leiva@gmail.com>
2025-04-09 12:35:01 +0100

    wip

--- clients/js/src/instructionPlansDraft/transactionPlannerDefault.ts
+++ clients/js/src/instructionPlansDraft/transactionPlannerDefault.ts
@@ -1,5 +1,24 @@
+import {
+  createTransactionMessage as kitCreateTransactionMessage,
+  pipe,
+  setTransactionMessageFeePayerSigner,
+  TransactionSigner,
+} from '@solana/kit';
 import { TransactionPlanner } from './transactionPlanner';
+import { createBaseTransactionPlannerFactory } from './transactionPlannerFactory';
+import { setTransactionMessageLifetimeUsingProvisoryBlockhash } from './transactionHelpers';
+import { updateOrPrependProvisorySetComputeUnitLimitInstruction } from './computeBudgetHelpers';
 
-export function createDefaultTransactionPlanner(): TransactionPlanner {
-  throw new Error('Not implemented');
+export function createDefaultTransactionPlanner(
+  feePayer: TransactionSigner
+): TransactionPlanner {
+  return createBaseTransactionPlannerFactory()({
+    createTransactionMessage: () =>
+      pipe(
+        kitCreateTransactionMessage({ version: 0 }),
+        setTransactionMessageLifetimeUsingProvisoryBlockhash,
+        updateOrPrependProvisorySetComputeUnitLimitInstruction,
+        (tx) => setTransactionMessageFeePayerSigner(feePayer, tx)
+      ),
+  });
 }
