0cfe9b4d3bbba4f10931e0d4b885658a55196908
Loris Leiva <loris.leiva@gmail.com>
2025-04-15 14:30:18 +0100

    wip

--- clients/js/test/createMetadata.test.ts
+++ clients/js/test/createMetadata.test.ts
@@ -13,6 +13,7 @@ import {
 import {
   createDefaultSolanaClient,
   createDeployedProgram,
+  createKeypairBuffer,
   generateKeyPairSignerWithSol,
 } from './_setup';
 
@@ -94,6 +95,48 @@ test('it creates a canonical metadata account with data larger than a transactio
   });
 });
 
+test('it creates a canonical metadata account using an existing buffer', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const authority = await generateKeyPairSignerWithSol(client);
+  const [program] = await createDeployedProgram(client, authority);
+
+  // And an existing buffer with the following data.
+  const data = getUtf8Encoder().encode('{"standard":"dummyIdl"}');
+  const buffer = await createKeypairBuffer(client, { payer: authority, data });
+
+  // When we create a canonical metadata account using the existing buffer.
+  const { metadata } = await createMetadata({
+    ...client,
+    payer: authority,
+    authority,
+    program,
+    seed: 'idl',
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    dataSource: DataSource.Direct,
+    format: Format.Json,
+    buffer: buffer.address,
+  });
+
+  // Then we expect the following metadata account to be created.
+  const account = await fetchMetadata(client.rpc, metadata);
+  t.like(account.data, <Metadata>{
+    discriminator: AccountDiscriminator.Metadata,
+    program,
+    authority: none(),
+    mutable: true,
+    canonical: true,
+    seed: 'idl',
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    format: Format.Json,
+    dataSource: DataSource.Direct,
+    dataLength: data.length,
+    data,
+  });
+});
+
 test('it creates a non-canonical metadata account', async (t) => {
   // Given the following authority and deployed program.
   const client = createDefaultSolanaClient();
@@ -169,5 +212,73 @@ test('it creates a non-canonical metadata account with data larger than a transa
     dataSource: DataSource.Direct,
     dataLength: largeData.length,
     data: largeData,
+  });
+});
+
+test('it creates a non-canonical metadata account using an existing buffer', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const authority = await generateKeyPairSignerWithSol(client);
+  const program = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+  // And an existing buffer with the following data.
+  const data = getUtf8Encoder().encode('{"standard":"dummyIdl"}');
+  const buffer = await createKeypairBuffer(client, { payer: authority, data });
+
+  // When we create a non-canonical metadata account using the existing buffer.
+  const { metadata } = await createMetadata({
+    ...client,
+    payer: authority,
+    authority,
+    program,
+    seed: 'idl',
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    dataSource: DataSource.Direct,
+    format: Format.Json,
+    buffer: buffer.address,
+  });
+
+  // Then we expect the following metadata account to be created.
+  const account = await fetchMetadata(client.rpc, metadata);
+  t.like(account.data, <Metadata>{
+    discriminator: AccountDiscriminator.Metadata,
+    program,
+    authority: some(authority.address),
+    mutable: true,
+    canonical: false,
+    seed: 'idl',
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    format: Format.Json,
+    dataSource: DataSource.Direct,
+    dataLength: data.length,
+    data,
+  });
+});
+
+test('it cannot create a metadata account if no data or buffer is provided', async (t) => {
+  // Given the following authority and deployed program.
+  const client = createDefaultSolanaClient();
+  const authority = await generateKeyPairSignerWithSol(client);
+  const program = address('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+  // When we try to create a metadata account without providing data or buffer.
+  const promise = createMetadata({
+    ...client,
+    payer: authority,
+    authority,
+    program,
+    seed: 'idl',
+    encoding: Encoding.Utf8,
+    compression: Compression.None,
+    dataSource: DataSource.Direct,
+    format: Format.Json,
+  });
+
+  // Then we expect the following error to be thrown.
+  await t.throwsAsync(promise, {
+    message:
+      'Either `buffer` or `data` must be provided to create a new metadata account.',
   });
 });
