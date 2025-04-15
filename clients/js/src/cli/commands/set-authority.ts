0a88223a47a01770a84fd9b8afec0228d34ad165
Loris Leiva <loris.leiva@gmail.com>
2025-04-15 16:31:25 +0100

    wip

--- clients/js/src/cli/commands/set-authority.ts
+++ clients/js/src/cli/commands/set-authority.ts
@@ -6,6 +6,7 @@ import { getPdaDetails } from '../../internals';
 import { programArgument, seedArgument } from '../arguments';
 import { GlobalOptions } from '../options';
 import { CustomCommand, getClient } from '../utils';
+import { logErrorAndExit } from '../logs';
 
 export function setSetAuthorityCommand(program: CustomCommand): void {
   program
@@ -15,33 +16,41 @@ export function setSetAuthorityCommand(program: CustomCommand): void {
     )
     .addArgument(seedArgument)
     .addArgument(programArgument)
-    .argument('<new-authority>', 'The new authority to set', address) // TODO: Make it a mandatory option to be explicit.
+    .requiredOption(
+      '--new-authority <new-authority>',
+      'The new authority to set',
+      (value: string): Address => {
+        try {
+          return address(value);
+        } catch {
+          logErrorAndExit(`Invalid new authority address: "${value}"`);
+        }
+      }
+    )
     .action(doSetAuthority);
 }
 
-type Options = {};
+type Options = { newAuthority: Address };
 async function doSetAuthority(
   seed: Seed,
   program: Address,
-  newAuthority: Address,
   _: Options,
   cmd: CustomCommand
 ) {
   const options = cmd.optsWithGlobals() as GlobalOptions & Options;
   const client = await getClient(options);
   const { metadata, programData } = await getPdaDetails({
-    rpc: client.rpc,
+    ...client,
     program,
-    authority: client.authority,
     seed,
   });
   await client.planAndExecute(
-    `Set additional authority on metadata account to ${chalk.bold(newAuthority)}`,
+    `Set additional authority on metadata account to ${chalk.bold(options.newAuthority)}`,
     sequentialInstructionPlan([
       getSetAuthorityInstruction({
         account: metadata,
         authority: client.authority,
-        newAuthority: address(newAuthority),
+        newAuthority: options.newAuthority,
         program,
         programData,
       }),
