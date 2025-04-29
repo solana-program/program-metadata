3666961230efe7ce1284010877ef941a7171c9c8
Loris Leiva <loris.leiva@gmail.com>
2025-04-29 11:17:41 +0100

    Fix npx CLI issue by using a more robust entrypoint (#15)

---
+++ clients/js/bin/cli.cjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S node
+
+const run = require('../dist/src/cli.js').run;
+
+run(process.argv);
--- clients/js/src/cli/index.ts
+++ clients/js/src/cli/index.ts
@@ -1,1 +1,16 @@
-export * from './program';
+import { logDebug, logError } from './logs';
+import { createProgram } from './program';
+
+const program = createProgram();
+
+export async function run(argv: readonly string[]) {
+  try {
+    await program.parseAsync(argv);
+  } catch (err) {
+    if (program.opts().debug) {
+      logDebug(`${(err as { stack: string }).stack}`);
+    }
+    logError((err as { message: string }).message);
+    process.exitCode = 1;
+  }
+}
